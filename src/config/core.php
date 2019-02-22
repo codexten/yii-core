3867c600b8b1a9ed0bb0a53843ff653e5ed3d1b0
Jomon Johnson <jomon.entero@gmail.com>
2019-02-22 14:52:15 +0530

    init

--- src/config/core.php
+++ src/config/core.php
@@ -7,5 +7,16 @@
  */
 
 return [
-    
+    'params' => $params + [
+            'mail.sender' => [
+                $params['mail.sender.email'] => $params['mail.sender.name'],
+            ],
+        ],
+    'aliases' => $aliases + [
+            '@bower' => '@vendor/bower-asset',
+            '@npm' => '@vendor/npm-asset',
+            '@vendor/bower' => '@vendor/bower-asset',
+            '@vendor/npm' => '@vendor/npm-asset',
+            '@_runtime' => '@root/runtime',
+        ],
 ];
