c652f691d0f2fbdb9d218c809619b19b52050473
Jomon Johnson <jomon.entero@gmail.com>
2019-07-24 14:35:04 +0530

    init

---
+++ src/db/ByDateRangeQueryTrait.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace codexten\yii\db;
+
+
+trait ByDateRangeQueryTrait
+{
+
+    /**
+     * @param $field
+     * @param $dateRange
+     *
+     * @return $this
+     */
+    public function byDateRange($field, $dateRange)
+    {
+        return $this->byTimestampRange("UNIX_TIMESTAMP($field)", $dateRange);
+    }
+
+    /**
+     * @param $field
+     * @param $dateRange
+     *
+     * @return $this
+     */
+    public function byTimestampRange($field, $dateRange)
+    {
+        if (!empty($dateRange) && strpos($dateRange, '-') !== false) {
+            list($start_date, $end_date) = explode(' - ', $dateRange);
+
+            $this->andFilterWhere([
+                'between',
+                $field,
+                strtotime($start_date),
+                strtotime($end_date),
+            ]);
+        }
+
+        return $this;
+
+    }
+}
