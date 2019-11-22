2cfac1c95a4771ae7988dcec2761130c04b9a6b4
Jomon Johnson <jomon.entero@gmail.com>
2019-11-22 17:41:03 +0530

    init

--- src/helpers/Url.php
+++ src/helpers/Url.php
@@ -10,7 +10,7 @@ class Url extends \yii\helpers\Url
 {
     public static function ensureUrl($url)
     {
-        if ($url !== Yii::$app->request->absoluteUrl) {
+        if (parse_url($url,PHP_URL_PATH) !== parse_url(Yii::$app->request->absoluteUrl,PHP_URL_PATH)) {
             return Yii::$app->response->redirect($url, 301);
         }
     }
